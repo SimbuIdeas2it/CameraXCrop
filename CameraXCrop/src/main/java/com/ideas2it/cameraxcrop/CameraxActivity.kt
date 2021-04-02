7f09e164006e72bf5de9ec97a38489e74306cfaa
Mac <mac@Macs-MacBook-Pro.local>
2021-04-02 13:46:55 +0530

    Save image feature added

--- CameraXCrop/src/main/java/com/ideas2it/cameraxcrop/CameraxActivity.kt
+++ CameraXCrop/src/main/java/com/ideas2it/cameraxcrop/CameraxActivity.kt
@@ -2,11 +2,18 @@ package com.ideas2it.cameraxcrop
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -18,6 +25,7 @@ import androidx.core.content.ContextCompat
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_camerax.*
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
@@ -146,6 +154,43 @@ class CameraxActivity : AppCompatActivity() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+        fun saveImage(itemImage: View, activity: Activity) {
+            val fileName: String
+            val imageFromView = getBitmapFromView(itemImage)
+
+            ByteArrayOutputStream().apply {
+                imageFromView.compress(Bitmap.CompressFormat.JPEG, 100, this)
+                fileName = UUID.nameUUIDFromBytes(this.toByteArray()).toString().replace("-", "")
+            }
+
+            val imageFile =  File("${activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)}/ChatOut/$fileName.jpg/")
+
+            if (!imageFile.exists()) {
+
+                val contentResolver = ContentValues().apply {
+                    put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis())
+                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                    put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+                }
+
+                activity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentResolver).apply {
+                    imageFromView.compress(Bitmap.CompressFormat.JPEG, 100, activity.contentResolver.openOutputStream(this!!))
+                }
+
+
+                Toast.makeText(activity, "saved", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(activity, "Already saved", Toast.LENGTH_SHORT).show()
+        }
+
+        fun getBitmapFromView(view: View): Bitmap {
+            return Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888).apply {
+                Canvas(this).apply {
+                    view.draw(this)
+                }
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -196,6 +241,7 @@ class CameraxActivity : AppCompatActivity() {
     fun start(activity: Activity) {
 
     }
+
 }
 private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
 
